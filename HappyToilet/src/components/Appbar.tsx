ad2884d51f115bf04aa9e607ed3a08f00a929eb5
oat47110 <33687611+SrnDev16@users.noreply.github.com>
2024-09-08 15:10:01 +0700

    finish
    v1 finished.

--- HappyToilet/src/components/Appbar.tsx
+++ HappyToilet/src/components/Appbar.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import logo from "../assets/images/logo.png";
 
 const Appbar = () => {
   return (
     <>
-      <div className="bg-yellow-500">fsdfsd</div>
+      <div className="bg-purple-400 min-h-16 flex justify-start items-center p-3">
+        <img src={logo} alt="Logo" className="w-12 mr-3" />
+        <p className="text-white">สำนักงานเขตพื้นที่การศึกษามัธยมศึกษาบึงกาฬ</p>
+      </div>
     </>
   );
 };
--- HappyToilet/src/components/Body.tsx
+++ HappyToilet/src/components/Body.tsx
@@ -1,19 +1,42 @@
-import React from 'react'
 import SchoolLink from "../data/LinkClip";
+import toilet from "../assets/images/toilet.jpg";
 
 const Body = () => {
   return (
     <>
-     {SchoolLink.map((data, index) => (
-        <div className="flex">
-          <p>{data.name}</p>{" "}
-          <p className="underline underline-offset-auto cursor-pointer">
-            <a href={data.linkVideo} target="_blank">{data.linkVideo}</a>
+      <div className="md:max-w-3xl mx-auto  p-2">
+        <div className="flex justify-center items-center my-2">
+          <img src={toilet} alt="" className="w-10 mr-2" />
+          <p>
+            รวม Link video โครงการ "สุขาดี มีความสุข" โรงเรียนในสังกัด
+            สพม.บึงกาฬ
           </p>
         </div>
-      ))}
+        <div className="table table-fixed w-full">
+          <div className="table-header-group">
+            <div className="table-row">
+              <div className="table-cell text-center border text-xl p-1">โรงเรียน</div>
+              <div className="table-cell text-center border text-xl p-1">Link</div>
+            </div>
+          </div>
+          <div className="table-row-group">
+            {SchoolLink.map((data, index) => (
+              <div className="table-row" key={index}>
+                <div className="table-cell border p-1">{data.name}</div>
+                <div className="table-cell border p-1">
+                  <p className="truncate underline text-blue-400">
+                    <a href={data.linkVideo} target="_blank">
+                      {data.linkVideo}
+                    </a>
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
